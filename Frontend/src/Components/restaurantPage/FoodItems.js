5eb57350ad112107f3144e2fe1938047c0861269
Harinath Prajapati <harinath.nitsri@mail.com>
2022-09-17 21:00:46 +0530

    UI changes of restaurant page

--- Frontend/src/Components/restaurantPage/FoodItems.js
+++ Frontend/src/Components/restaurantPage/FoodItems.js
@@ -5,7 +5,7 @@ import "./RestaurantNav.css";
 function FoodItems(props) {
   const { item } = props;
   const [inCart, setInCart] = useState(props.inCart);
-  
+
   const addToCart = (e) => {
     e.preventDefault();
     props.addToCart(props.item);
@@ -14,22 +14,21 @@ function FoodItems(props) {
 
   return (
     <div className="col-sm-3">
-      <figure className="card card-product">
-        <div className="img-wrap">
-          <img
-            className="img"
-            alt="product pic not availalble"
-            src={item.img}
-            width="341px"
-            height="250px"
-          ></img>
-        </div>
+      <figure id="product-card" className="card card-product">
+        <img
+          className="img-wrap"
+          alt="product pic not availalble"
+          src={item.img}
+          width="341px"
+          height="250px"
+        ></img>
+
         <span id="item_name" className="navbar-brand">
           {item.name}
         </span>
-        <span>{item.desc}</span>
-        <b>{item.location}</b>
-        <b className="navbar-brand">Rs.{item.price}</b>
+        <span className="item_desc">{item.desc}</span>
+        <b className="item_location">{item.location}</b>
+        <b id="item_price" className="navbar-brand">Rs.{item.price}</b>
         <div className="bottom-wrap">
           {inCart ? (
             <span className="btn btn-success">Added into Cart</span>
